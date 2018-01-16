e3427f04971b04daf0814a49b5bcdab25bee2698
Anton Drukh <anton@snyk.io>
2018-01-16 17:02:19 +0200

    basic project path traversal

--- src/snykeclipseplugin/handlers/HelloWorldHandler.java
+++ src/snykeclipseplugin/handlers/HelloWorldHandler.java
@@ -6,6 +6,8 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.resources.IProject;
 
 /** <b>Warning</b> : 
 As explained in <a href="http://wiki.eclipse.org/Eclipse4/RCP/FAQ#Why_aren.27t_my_handler_fields_being_re-injected.3F">this wiki page</a>, it is not recommended to define @Inject fields in a handler. <br/><br/>
@@ -15,10 +17,15 @@ public class HelloWorldHandler {
 
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell s) {
-		
-		MessageDialog.openInformation(s, "E4 Information Dialog", "Hello world from a pure Eclipse 4 plug-in");
+		String pathsScanned = "";
+		// iterate over projects in workspace
+		for (IProject project: ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
+			// fetch path to main pom.xml file of project
+			final String pomXmlPath = project.getFile("pom.xml").getFullPath().toOSString();
+			// run via API
+			pathsScanned += pomXmlPath + ", ";
+		};
 
+		MessageDialog.openInformation(s, "Snyk", "Paths scanned: " + pathsScanned);
 	}
-
-
 }
