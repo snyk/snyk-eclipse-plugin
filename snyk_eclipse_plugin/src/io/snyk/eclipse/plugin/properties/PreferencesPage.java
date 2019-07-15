ced12ae77226f87d2002b35a33834a79e16ae567
bmvermeer <->
2019-07-15 20:42:42 +0200

    change text for pref page

--- snyk_eclipse_plugin/src/io/snyk/eclipse/plugin/properties/PreferencesPage.java
+++ snyk_eclipse_plugin/src/io/snyk/eclipse/plugin/properties/PreferencesPage.java
@@ -16,7 +16,7 @@ public class PreferencesPage extends FieldEditorPreferencePage implements IWorkb
 	public void init(IWorkbench workbench) {
        setPreferenceStore(Preferences.STORE);
        setMessage("Snyk preferences");
-       setDescription("- Auth token can be found in you account view \n- Path is the path to you package mangers (npm, maven, etc.) separated by : \n");
+       setDescription("- Please \"authenticate\" this plugin by clicking the button \n- Path is the path to your package managers (npm, maven, etc.) separated by : \n");
 	}
 
 	@Override
