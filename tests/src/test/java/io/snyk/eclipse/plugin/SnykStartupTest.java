837f3134be988f19c5c93aba6c71a717aa7a4edb
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-07-12 10:55:08 +0100

    fix: broken test

--- tests/src/test/java/io/snyk/eclipse/plugin/SnykStartupTest.java
+++ tests/src/test/java/io/snyk/eclipse/plugin/SnykStartupTest.java
@@ -3,6 +3,8 @@ package io.snyk.eclipse.plugin;
 import org.eclipse.core.runtime.ILog;
 import org.junit.jupiter.api.Test;
 
+import io.snyk.eclipse.plugin.properties.Preferences;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -12,8 +14,21 @@ class SnykStartupTest {
   void testIsDownloadAllowed() {
     SnykStartup cut = new SnykStartup();
     cut.setLogger(mock(ILog.class));
-    assertTrue(cut.isDownloadAllowed(""));
-    assertTrue(cut.isDownloadAllowed(null));
-    assertFalse(cut.isDownloadAllowed("a"));
+    Preferences p = new Preferences();
+    p.store(Preferences.MANAGE_BINARIES_AUTOMATICALLY, "true");
+
+    
+    p.store(Preferences.LS_BINARY_KEY, "");
+    assertTrue(cut.isDownloadAllowed(p));
+    
+    p.store(Preferences.MANAGE_BINARIES_AUTOMATICALLY, "true");
+    assertFalse(cut.isDownloadAllowed(p));
+
+    p.store(Preferences.MANAGE_BINARIES_AUTOMATICALLY, "true");
+    p.store(Preferences.LS_BINARY_KEY, null);
+    assertTrue(cut.isDownloadAllowed(p));
+    
+    p.store(Preferences.LS_BINARY_KEY, "a");
+    assertFalse(cut.isDownloadAllowed(p));
   }
 }
