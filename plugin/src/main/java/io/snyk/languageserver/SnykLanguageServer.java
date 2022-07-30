2147af851646bcac2eff485f026407cd9a056a6a
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-07-30 11:23:49 -0500

    feat: ✨ automatically start workspace scan when starting Eclipse [ROAD-969]
    - removed our custom progress reporting in favor of LSP4e progress reporting

--- plugin/src/main/java/io/snyk/languageserver/SnykLanguageServer.java
+++ plugin/src/main/java/io/snyk/languageserver/SnykLanguageServer.java
@@ -5,23 +5,34 @@ import io.snyk.eclipse.plugin.properties.preferences.Preferences;
 import io.snyk.eclipse.plugin.utils.Lists;
 import io.snyk.eclipse.plugin.utils.SnykLogger;
 import org.apache.commons.lang3.SystemUtils;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.lsp4e.LSPEclipseUtils;
+import org.eclipse.lsp4e.LanguageServerWrapper;
+import org.eclipse.lsp4e.LanguageServersRegistry;
+import org.eclipse.lsp4e.LanguageServiceAccessor;
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
 import org.eclipse.lsp4e.server.StreamConnectionProvider;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.ui.PlatformUI;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
-public class SnykStreamConnectionProvider extends ProcessStreamConnectionProvider implements StreamConnectionProvider {
+public class SnykLanguageServer extends ProcessStreamConnectionProvider implements StreamConnectionProvider {
   public static final String LANGUAGE_SERVER_ID = "io.snyk.languageserver";
   private final LsRuntimeEnvironment runtimeEnvironment;
 
-  public SnykStreamConnectionProvider() {
+  public SnykLanguageServer() {
     runtimeEnvironment = new LsRuntimeEnvironment();
   }
 
@@ -36,7 +47,7 @@ public class SnykStreamConnectionProvider extends ProcessStreamConnectionProvide
     }
 
     List<String> commands = Lists.of(Preferences.getInstance().getLsBinary(), "-l", "info", "-f",
-      new File(Preferences.getInstance().getLsBinary()).getParent() + File.separator + "snyk-ls.log");
+        new File(Preferences.getInstance().getLsBinary()).getParent() + File.separator + "snyk-ls.log");
     String workingDir = SystemUtils.USER_DIR;
     setCommands(commands);
     setWorkingDirectory(workingDir);
@@ -64,12 +75,27 @@ public class SnykStreamConnectionProvider extends ProcessStreamConnectionProvide
 
   @Override
   public Object getInitializationOptions(URI rootUri) {
-	LsConfigurationUpdater.Settings currentSettings = null;
-	try {
-    currentSettings = new LsConfigurationUpdater().getCurrentSettings();
-	} catch (RuntimeException e) {
-         SnykLogger.logError(e);
+    LsConfigurationUpdater.Settings currentSettings = null;
+    try {
+      currentSettings = new LsConfigurationUpdater().getCurrentSettings();
+    } catch (RuntimeException e) {
+      SnykLogger.logError(e);
     }
-	return currentSettings;
+    return currentSettings;
+  }
+
+  public static void InitializeServer() {
+    var projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+    for (IProject project : projects) {
+      if (project.isAccessible()) {
+        try {
+          LanguageServiceAccessor.getLSWrapper(project,
+              LanguageServersRegistry.getInstance().getDefinition(SnykLanguageServer.LANGUAGE_SERVER_ID));
+        } catch (IOException e) {
+          SnykLogger.logError(e);
+          return;
+        }
+      }
+    }
   }
 }
