684cc72c71b3d3f029a5c0a7794f766ccec9bfd1
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-08-01 11:23:16 -0500

    chore: update language server protocol version

--- plugin/src/main/java/io/snyk/languageserver/download/LsBinaries.java
+++ plugin/src/main/java/io/snyk/languageserver/download/LsBinaries.java
@@ -4,13 +4,13 @@ import java.net.URI;
 
 public class LsBinaries {
   private static final String LS_DOWNLOAD_BASE_URL = "https://static.snyk.io/snyk-ls";
-  public static final String REQUIRED_LSP_VERSION = "2";
+  public static final String REQUIRED_LS_PROTOCOL_VERSION = "3";
 
   public static URI getBaseUri() {
-    return URI.create(String.format("%s/%s", LS_DOWNLOAD_BASE_URL, REQUIRED_LSP_VERSION));
+    return URI.create(String.format("%s/%s", LS_DOWNLOAD_BASE_URL, REQUIRED_LS_PROTOCOL_VERSION));
   }
 
   public static URI getAssetUri(String assetName) {
-    return URI.create(String.format("%s/%s/%s", LS_DOWNLOAD_BASE_URL, REQUIRED_LSP_VERSION, assetName));
+    return URI.create(String.format("%s/%s/%s", LS_DOWNLOAD_BASE_URL, REQUIRED_LS_PROTOCOL_VERSION, assetName));
   }
 }
