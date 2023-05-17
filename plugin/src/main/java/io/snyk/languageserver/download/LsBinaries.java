2335a55f4e869aba8c2bc3570c20fa4d59e7a290
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2023-05-17 10:15:26 +0200

    feat: increase language server version

--- plugin/src/main/java/io/snyk/languageserver/download/LsBinaries.java
+++ plugin/src/main/java/io/snyk/languageserver/download/LsBinaries.java
@@ -4,7 +4,7 @@ import java.net.URI;
 
 public class LsBinaries {
   private static final String LS_DOWNLOAD_BASE_URL = "https://static.snyk.io/snyk-ls";
-  public static final String REQUIRED_LS_PROTOCOL_VERSION = "9";
+  public static final String REQUIRED_LS_PROTOCOL_VERSION = "10";
 
   public static URI getBaseUri() {
     return URI.create(String.format("%s/%s", LS_DOWNLOAD_BASE_URL, REQUIRED_LS_PROTOCOL_VERSION));
