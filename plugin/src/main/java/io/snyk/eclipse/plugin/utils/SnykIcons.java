8f2893ee85e7a77f61aadb9f928842a908fe5c0b
Knut Funkel <knut.funkel@snyk.io>
2024-11-18 17:04:02 +0100

    fix: remove unneeded ImageDescriptorProvider

--- plugin/src/main/java/io/snyk/eclipse/plugin/utils/SnykIcons.java
+++ plugin/src/main/java/io/snyk/eclipse/plugin/utils/SnykIcons.java
@@ -2,31 +2,23 @@ package io.snyk.eclipse.plugin.utils;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 
+import io.snyk.eclipse.plugin.Activator;
+
 public class SnykIcons {
-    private static ImageDescriptorProvider imageProvider;
+	public static final ImageDescriptor CODE = Activator.getImageDescriptor("/icons/code.png");
+	public static final ImageDescriptor CODE_DISABLED = Activator.getImageDescriptor("/icons/code_disabled.png");
 
-    static {
-        imageProvider = new ActivatorImageDescriptorProvider();
-    }
+	public static final ImageDescriptor OSS = Activator.getImageDescriptor("/icons/oss.png");
+	public static final ImageDescriptor OSS_DISABLED = Activator.getImageDescriptor("/icons/oss_disabled.png");
 
-    public static final ImageDescriptor CODE = imageProvider.getImageDescriptor("/icons/code.png");
-    public static final ImageDescriptor CODE_DISABLED = imageProvider.getImageDescriptor("/icons/code_disabled.png");
+	public static final ImageDescriptor IAC = Activator.getImageDescriptor("/icons/iac.png");
+	public static final ImageDescriptor IAC_DISABLED = Activator.getImageDescriptor("/icons/iac_disabled.png");
 
-    public static final ImageDescriptor OSS = imageProvider.getImageDescriptor("/icons/oss.png");
-    public static final ImageDescriptor OSS_DISABLED = imageProvider.getImageDescriptor("/icons/oss_disabled.png");
+	public static final ImageDescriptor SEVERITY_CRITICAL = Activator
+			.getImageDescriptor("/icons/severity-critical.png");
+	public static final ImageDescriptor SEVERITY_HIGH = Activator.getImageDescriptor("/icons/severity-high.png");
+	public static final ImageDescriptor SEVERITY_MEDIUM = Activator.getImageDescriptor("/icons/severity-medium.png");
+	public static final ImageDescriptor SEVERITY_LOW = Activator.getImageDescriptor("/icons/severity-low.png");
 
-    public static final ImageDescriptor IAC = imageProvider.getImageDescriptor("/icons/iac.png");
-    public static final ImageDescriptor IAC_DISABLED = imageProvider.getImageDescriptor("/icons/iac_disabled.png");
-
-    public static final ImageDescriptor SEVERITY_CRITICAL = imageProvider.getImageDescriptor("/icons/severity-critical.png");
-    public static final ImageDescriptor SEVERITY_HIGH = imageProvider.getImageDescriptor("/icons/severity-high.png");
-    public static final ImageDescriptor SEVERITY_MEDIUM = imageProvider.getImageDescriptor("/icons/severity-medium.png");
-    public static final ImageDescriptor SEVERITY_LOW = imageProvider.getImageDescriptor("/icons/severity-low.png");
-
-    public static final ImageDescriptor ENABLED = imageProvider.getImageDescriptor("/icons/enabled.png");
-
-    // You can add a method to set a custom ImageDescriptorProvider if needed
-    public static void setImageDescriptorProvider(ImageDescriptorProvider provider) {
-        imageProvider = provider;
-    }
+	public static final ImageDescriptor ENABLED = Activator.getImageDescriptor("/icons/enabled.png");
 }
