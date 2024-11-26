a13b160b2ee5ebe58e04d7780cd76e3b8d6bad62
Knut Funkel <knut.funkel@snyk.io>
2024-11-26 09:35:25 +0100

    fix: refactor collapse tree handler

--- plugin/src/main/java/io/snyk/eclipse/plugin/views/snyktoolview/handlers/CollapseTreeHandler.java
+++ plugin/src/main/java/io/snyk/eclipse/plugin/views/snyktoolview/handlers/CollapseTreeHandler.java
@@ -3,15 +3,8 @@ package io.snyk.eclipse.plugin.views.snyktoolview.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 
-import io.snyk.eclipse.plugin.utils.SnykLogger;
-import io.snyk.eclipse.plugin.utils.SnykMessageDialog;
-import io.snyk.eclipse.plugin.views.snyktoolview.ISnykToolView;
-import io.snyk.eclipse.plugin.views.snyktoolview.SnykToolView;
+import io.snyk.eclipse.plugin.SnykStartup;
 
 public class CollapseTreeHandler extends AbstractHandler {
 
@@ -22,27 +15,13 @@ public class CollapseTreeHandler extends AbstractHandler {
 
 		switch (commandId) {
 		case "io.snyk.eclipse.plugin.commands.TreeCollapse":
-			getView().getTreeViewer().collapseAll();
+			SnykStartup.getView().getTreeViewer().collapseAll();
 			break;
 		case "io.snyk.eclipse.plugin.commands.TreeExpand":
-			getView().getTreeViewer().expandAll();
+			SnykStartup.getView().getTreeViewer().expandAll();
 			break;
 		}
 
 		return null;
-	}
-
-	public ISnykToolView getView() {
-
-		IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		ISnykToolView toolView;
-		try {
-			toolView = (ISnykToolView) activePage.showView(SnykToolView.ID);
-		} catch (PartInitException e) {
-			SnykLogger.logError(e);
-			return null;
-		}
-		return toolView;
-
 	}
 }
