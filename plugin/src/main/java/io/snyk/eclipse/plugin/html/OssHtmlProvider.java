7acbe7df40914300133ac872f169b9e878192075
Bastian Doetsch <bastian.doetsch@snyk.io>
2025-01-29 10:04:17 +0100

    chore: add github linting step

--- plugin/src/main/java/io/snyk/eclipse/plugin/html/OssHtmlProvider.java
+++ plugin/src/main/java/io/snyk/eclipse/plugin/html/OssHtmlProvider.java
@@ -1,24 +1,22 @@
 package io.snyk.eclipse.plugin.html;
 
 public class OssHtmlProvider extends BaseHtmlProvider {
-	private static OssHtmlProvider instance = new OssHtmlProvider();
+    private static OssHtmlProvider instance = new OssHtmlProvider();
 
-	public static OssHtmlProvider getInstance() {
-		synchronized (OssHtmlProvider.class) {
-			if (instance == null) {
-				if (instance == null) {
-					instance = new OssHtmlProvider();
-				}
-			}
-		}
-		return instance;
-	}
+    public static OssHtmlProvider getInstance() {
+        synchronized (OssHtmlProvider.class) {
+            if (instance == null) {
+                instance = new OssHtmlProvider();
+            }
+        }
+        return instance;
+    }
 
-	@Override
-	public String replaceCssVariables(String html) {
-		html = super.replaceCssVariables(html);
-		html = html.replace("var(--container-background-color)",
-				super.getColorAsHex("org.eclipse.ui.workbench.DARK_BACKGROUND", "#F0F0F0"));
-		return html;
-	}
+    @Override
+    public String replaceCssVariables(String html) {
+        html = super.replaceCssVariables(html);
+        html = html.replace("var(--container-background-color)",
+            super.getColorAsHex("org.eclipse.ui.workbench.DARK_BACKGROUND", "#F0F0F0"));
+        return html;
+    }
 }
