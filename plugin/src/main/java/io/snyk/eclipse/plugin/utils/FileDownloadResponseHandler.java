20778e0b116a4ab815591106b5df2d6283a3a7c8
Bastian Doetsch <bastian.doetsch@snyk.io>
2025-03-11 10:53:33 +0100

    fix: a download bug

--- plugin/src/main/java/io/snyk/eclipse/plugin/utils/FileDownloadResponseHandler.java
+++ plugin/src/main/java/io/snyk/eclipse/plugin/utils/FileDownloadResponseHandler.java
@@ -12,31 +12,30 @@ import org.eclipse.core.runtime.SubMonitor;
 
 public class FileDownloadResponseHandler implements ResponseHandler<File> {
 
-	private final File destinationFile;
-	private final IProgressMonitor progressMonitor;
+  private final File destinationFile;
+  private final IProgressMonitor progressMonitor;
 
-	public FileDownloadResponseHandler(File file, IProgressMonitor monitor) {
-		this.destinationFile = file;
-		this.progressMonitor = monitor;
-	}
+  public FileDownloadResponseHandler(File file, IProgressMonitor monitor) {
+    this.destinationFile = file;
+    this.progressMonitor = monitor;
+  }
 
-	@Override
-	public File handleResponse(HttpResponse httpResponse) throws IOException {
-		long contentLengthStr = httpResponse.getEntity().getContentLength();
-		SubMonitor subMonitor = SubMonitor.convert(progressMonitor, 100);
-		InputStream inputStream = httpResponse.getEntity().getContent();
+  @Override
+  public File handleResponse(HttpResponse httpResponse) throws IOException {
+    long contentLengthStr = httpResponse.getEntity().getContentLength();
+    SubMonitor subMonitor = SubMonitor.convert(progressMonitor, 100);
+    InputStream inputStream = httpResponse.getEntity().getContent();
 
-		try (var outputStream = Files.newOutputStream(destinationFile.toPath())) {
-			int readCount = 0;
-			byte[] buffer = new byte[1024];
+    try (var outputStream = Files.newOutputStream(destinationFile.toPath())) {
+      int readCount;
+      byte[] buffer = new byte[1024];
 
-			while (readCount != -1) {
-				readCount = inputStream.read(buffer);
-				outputStream.write(buffer, 0, readCount);
-				subMonitor.split(Math.round((float) readCount / (float) contentLengthStr));
-			}
-		}
+      while ((readCount = inputStream.read(buffer)) != -1) {
+        outputStream.write(buffer, 0, readCount);
+        subMonitor.split(Math.round((float)readCount / (float)contentLengthStr));
+      }
+    }
 
-		return this.destinationFile;
-	}
+    return this.destinationFile;
+  }
 }
