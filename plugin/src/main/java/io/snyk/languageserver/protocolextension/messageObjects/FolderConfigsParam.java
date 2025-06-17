b14d4a68c31a9d9b578e367ae7fccd7042ecd848
Ben Durrans <Benjamin.Durrans@snyk.io>
2025-06-17 16:38:42 +0100

    fix: remove dependency on GSON [IDE-1132] (#306)
    The dependency was causing an issue for a customer. We didn't really need it anyway.

--- plugin/src/main/java/io/snyk/languageserver/protocolextension/messageObjects/FolderConfigsParam.java
+++ plugin/src/main/java/io/snyk/languageserver/protocolextension/messageObjects/FolderConfigsParam.java
@@ -2,11 +2,8 @@ package io.snyk.languageserver.protocolextension.messageObjects;
 
 import java.util.List;
 
-import com.google.gson.annotations.SerializedName;
-
 public class FolderConfigsParam {
 
-	@SerializedName("folderConfigs")
 	private List<FolderConfig> folderConfigs;
 
 	public FolderConfigsParam(List<FolderConfig> folderConfigs) {
