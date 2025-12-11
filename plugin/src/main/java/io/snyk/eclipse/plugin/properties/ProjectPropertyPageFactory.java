be2d37d55be0fce0be253e0d3dae7cb6de670f00
Andrew Robinson Hodges <andrew.robinsonhodges@snyk.io>
2025-12-11 16:17:39 +0000

    refactor: remove unused code

--- plugin/src/main/java/io/snyk/eclipse/plugin/properties/ProjectPropertyPageFactory.java
+++ plugin/src/main/java/io/snyk/eclipse/plugin/properties/ProjectPropertyPageFactory.java
@@ -1,8 +1,6 @@
 package io.snyk.eclipse.plugin.properties;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.core.runtime.IExecutableExtensionFactory;
 
 import io.snyk.eclipse.plugin.preferences.Preferences;
@@ -11,17 +9,7 @@ import io.snyk.eclipse.plugin.preferences.Preferences;
  * Factory that creates either the redirect page (for HTML settings mode) or 
  * the native project property page based on the USE_LS_HTML_CONFIG_DIALOG preference.
  */
-public class ProjectPropertyPageFactory implements IExecutableExtensionFactory, IExecutableExtension {
-
-	private IConfigurationElement config;
-	private String propertyName;
-
-	@Override
-	public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
-			throws CoreException {
-		this.config = config;
-		this.propertyName = propertyName;
-	}
+public class ProjectPropertyPageFactory implements IExecutableExtensionFactory {
 
 	@Override
 	public Object create() throws CoreException {
