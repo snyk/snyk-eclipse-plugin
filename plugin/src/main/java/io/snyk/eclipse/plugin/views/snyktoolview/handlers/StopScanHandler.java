997220667040994afe7aa4796beeb773871bdbf2
nick-y-snyk <nikita.yasnohorodskyi@snyk.io>
2026-06-12 21:13:09 +0200

    refactor: drop SWT tree, SnykIssueCache, and call getIssueDescription directly [IDE-2126] (#416)
    * refactor: drop SWT tree, SnykIssueCache, and call getIssueDescription directly [IDE-2126]
    
    * fix: remove duplicate bundle entries from MANIFEST.MF [IDE-2126]
    
    * fix: remove unused Set import from SnykExtendedLanguageClient [IDE-2126]
    
    * test: add snykScan coverage for refreshBrowser dispatch path [IDE-2126]
    
    * refactor: rename IHandlerCommands to HandlerCommands [IDE-2126]
    
    * fix: add comment explaining Secrets maps to Code HTML provider [IDE-2126]
    
    * refactor: drop unused DIAGNOSTIC_SOURCE_SNYK_* constants [IDE-2126]
    
    * fix: restore error HTML display when scan fails with presentableError [IDE-2126]
    
    snykScan now passes the full SnykScanParam to refreshBrowser so the
    presentableError payload is not discarded. BrowserHandler.setBrowserText
    and the error branch in SnykToolView.refreshBrowser restore the
    getErrorHtml path that was lost during the SWT tree refactor.

--- plugin/src/main/java/io/snyk/eclipse/plugin/views/snyktoolview/handlers/StopScanHandler.java
+++ plugin/src/main/java/io/snyk/eclipse/plugin/views/snyktoolview/handlers/StopScanHandler.java
@@ -3,35 +3,13 @@ package io.snyk.eclipse.plugin.views.snyktoolview.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-
-import io.snyk.eclipse.plugin.utils.SnykLogger;
-import io.snyk.eclipse.plugin.views.snyktoolview.ISnykToolView;
-import io.snyk.eclipse.plugin.views.snyktoolview.SnykToolView;
-import io.snyk.languageserver.ScanState;
 import io.snyk.languageserver.protocolextension.SnykExtendedLanguageClient;
 
 public class StopScanHandler extends AbstractHandler {
 
-	private ISnykToolView toolView;
-
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		SnykExtendedLanguageClient.getInstance().getProgressManager().cancelAll();
-		ScanState.getInstance().clearAllScanStates();
-		PlatformUI.getWorkbench().getDisplay().asyncExec(() -> {
-			IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-			try {
-				toolView = (ISnykToolView) activePage.showView(SnykToolView.ID);
-				toolView.clearTree();
-				toolView.refreshTree();
-			} catch (PartInitException e) {
-				SnykLogger.logError(e);
-			}
-		});
-
 		return null;
 	}
 }
