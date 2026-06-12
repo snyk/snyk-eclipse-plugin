ad0ebecfdf7acb3bebf5d1038ea0f7bc44a01ef4
Nick Yasnohorodskyi <nikita.yasnohorodskyi@snyk.io>
2026-06-12 20:25:59 +0200

    fix: remove unused Set import from SnykExtendedLanguageClient [IDE-2126]

--- plugin/src/main/java/io/snyk/languageserver/protocolextension/SnykExtendedLanguageClient.java
+++ plugin/src/main/java/io/snyk/languageserver/protocolextension/SnykExtendedLanguageClient.java
@@ -14,7 +14,6 @@ import java.util.Objects;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
